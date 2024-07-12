8226ed991caa5130c152385187d76ad2e3a9a429
Kevin Mawuntu <kevinmawuntu123@gmail.com>
2024-07-12 21:01:53 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, aut!</p>
+    <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+    </ul>
 </body>
 </html>
